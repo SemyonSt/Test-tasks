bf780abfa431bc98903035261f12053bffdcc679
Семён <sema.stulnikov@mail.ru>
2023-07-03 16:01:00 +0300

    all

---
+++ 02-store/data.js
@@ -0,0 +1,212 @@
+export const man = [
+  {
+    name: 'Мужская рубашка',
+    date: '01.01.2022',
+    description: 'Ткань: хлопок 100% \n Страна: Турция \n Серия: custom',
+    img: './img/man/DSC_0170.jpg',
+  },
+  {
+    name: 'Мужская длинная рубашка',
+    date: '15.12.2021',
+    description: 'Ткань: хлопок 100% \n Страна: Турция \n Серия: custom',
+    img: './img/man/1.jpg',
+  },
+  {
+    name: 'Мужская футболка',
+    date: '5.10.2020',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/man/orig.webp',
+  },
+  {
+    name: 'Футболка полло',
+    date: '1.02.2022',
+    description: 'Ткань: хлопок 100% \n Страна: Великобритания \n Серия: custom',
+    img: './img/man/100027449974b0.webp',
+  },
+  {
+    name: 'Мужская куртка',
+    date: '15.12.2021',
+    description: 'Ткань: полиэстр \n Страна: Китай \n Серия: custom',
+    img: './img/man/100029628841b0.webp',
+  },
+  {
+    name: 'Джинсовая куртка',
+    date: '1.02.2023',
+    description: 'Ткань: Джинс \n США: Турция \n Серия: custom',
+    img: './img/man/Sa568a444be1440cdbf629e8d8cfabbf3w.jpg_640x640.webp',
+  },
+  {
+    name: 'Мужския брюки',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Турция \n Серия: custom',
+    img: './img/man/TR-0327-WHITE-6-1.jpg',
+  },
+  {
+    name: 'Мужская олимпийка',
+    date: '1.02.2023',
+    description: 'Ткань: полиэстр \n Страна: Турция \n Серия: custom',
+    img: './img/man/1bb8767ea83972baae24bc7d434985236748004f.webp',
+  },
+  {
+    name: 'Кеды',
+    date: '1.02.2023',
+    description: 'Ткань: кожа \n Страна: Турция \n Серия: custom',
+    img: './img/man/Seb99875f77f84eb4bcff191393412a211.jpg_480x480.webp',
+  },
+  {
+    name: 'Меховая шапка',
+    date: '1.02.2023',
+    description: 'Ткань: натуральный мех \n Страна: Россия \n Серия: custom',
+    img: './img/man/sb-shabpki-new-34.jpg',
+  },
+  {
+    name: 'Штаны',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Турция \n Серия: custom',
+    img: './img/man/ca7m2s1g27ojdv7e24eg.jpg',
+  }
+
+]
+
+export const woman = [
+  {
+    name: 'Женская длинная рубашка',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/women/t1.jpg',
+  },
+  {
+    name: 'Женская укороченная рубашка',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/women/images.jpeg',
+  },
+  {
+    name: 'Юбка',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/women/202101181825514178851.jpg',
+  },
+  {
+    name: 'Юбка мини',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/women/S1952b34ef8e5487ab664c59383b7768cy.webp',
+  },
+  {
+    name: 'Платье',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/women/HTB13YUfVVzqK1RjSZFvq6AB7VXaT.jpg',
+  },
+  {
+    name: 'Длинное платье',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/women/1.jpg',
+  },
+  {
+    name: 'Джинсовая куртка',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/women/S69bb6fdc6bd2454c9f9f55de2d7a77f03.jpg_480x480.webp',
+  },
+  {
+    name: 'Брюки женские',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Турция \n Серия: custom',
+    img: './img/women/8a9fb77d22a6e5b44d9e6e5402807b62667e9ec4_original.jpeg',
+  },
+  {
+    name: 'Кепка',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Китай \n Серия: custom.',
+    img: './img/women/H8652389b927a47a7b79c509cad3bca75B.jpg_480x480.webp',
+  },
+  {
+    name: 'Вязаная шапка',
+    date: '1.02.2023',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/women/2.jpg',
+  },
+  {
+    name: 'Кеды',
+    date: '1.02.2023',
+    description: 'Ткань: кожа \n США: Россия \n Серия: custom',
+    img: './img/women/S441941dc8020445db8ae2d853671949ab.webp',
+  },
+  {
+    name: 'Туфли на каблуке',
+    date: '1.02.2023',
+    description: 'Ткань: кожа \n Страна: Россия \n Серия: custom',
+    img: './img/women/912f9c9f-1e7a-4590-85e3-382f2e6171da.jpg',
+  }
+
+
+]
+
+export const children = [
+  {
+    name: 'Детская рубашка',
+    date: '1.02.2022',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/children/BSU000144-1-01-300Wx300H.jpg',
+  },
+  {
+    name: 'Детская футболка',
+    date: '5.12.2020',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/children/193816.970.jpg',
+  },
+  {
+    name: 'Худи для девочек',
+    date: '5.5.2020',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/children/03576102_14_1000x1000.jpg',
+  },
+  {
+    name: 'Рубашка поло',
+    date: '5.11.2020',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/children/639899c4041bb966670137.jpg',
+  },
+  {
+    name: 'Зимняя куртка для девочек',
+    date: '5.3.2020',
+    description: 'Ткань: тексиль/полиэстр \n Страна: Россия \n Серия: custom',
+    img: './img/children/a2d15193d0946bbefd21e991db2970450e3f1ec5_original.jpeg',
+  },
+  {
+    name: 'Зимняя куртка для мальчиков',
+    date: '5.12.2020',
+    description: 'Ткань: тексиль/полиэстр \n Страна: Россия \n Серия: custom',
+    img: './img/children/1.jpg',
+  },
+  {
+    name: 'Шапка для девочек',
+    date: '1.2.2020',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/children/123kk.jpeg',
+  },
+  {
+    name: 'Кепка с принтом "Simba"',
+    date: '1.1.2020',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/children/1rrr.jpg',
+  },
+  {
+    name: 'Кепка',
+    date: '5.7.2022',
+    description: 'Ткань: хлопок 100% \n Страна: Россия \n Серия: custom',
+    img: './img/children/kepka2.jpg',
+  },
+  {
+    name: 'Кеды детские',
+    date: '5.4.2020',
+    description: 'Ткань: кожа \n Страна: Россия \n Серия: custom',
+    img: './img/children/lacoste-t-clip.jpg',
+  }
+
+
+
+]
