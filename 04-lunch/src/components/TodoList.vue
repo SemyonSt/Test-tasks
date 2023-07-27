3b18bddc8cc1334fc507d0ec3bf49e65a7ad37d9
Семён <sema.stulnikov@mail.ru>
2023-07-27 20:49:33 +0300

    to-do

---
+++ 04-lunch/src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <TodoItem
+      v-for="(todo, i) of todos"
+      v-bind:index="i"
+      v-bind:todo="todo"
+      v-bind:key="todo.id"
+      v-on:remove-todo="removeTodo"
+    />
+  </ul>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+
+export default {
+  props: ["todos"],
+  name: "App",
+  components: {
+    TodoItem,
+  },
+  methods: {
+    removeTodo(id) {
+      console.log(id)
+      this.$emit('remove-todo', id)
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
